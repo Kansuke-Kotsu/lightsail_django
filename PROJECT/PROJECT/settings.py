a5e9b7e9c481655baa00d85836d2f558ada7e2d7
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2025-05-11 18:42:41 +0900

    Update settings.py

--- PROJECT/PROJECT/settings.py
+++ PROJECT/PROJECT/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#54f$%m4s1p7of!+ve%wwr5sq!6b%&ckjg2gble=c)7_0n-0jq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['konjiki-no-kansuke.com', 'www.konjiki-no-kansuke.com', 'localhost', '127.0.0.1']
 
 #DEBUG = True
@@ -131,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
